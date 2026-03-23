a9da0f30994e2650c9c1bb59a4507e01f0671a44
Xinyu Bao <34500975+AspadaX@users.noreply.github.com>
2026-03-23 16:36:35 +0800

    Refactor block query to use enum instead of filter struct

--- backend/src/databases/database/traits/blocks.rs
+++ backend/src/databases/database/traits/blocks.rs
@@ -5,25 +5,22 @@ use crate::models::block::Block;
 
 #[async_trait]
 pub trait Blocks {
-    /// Create 
+    /// Create
     async fn create_blocks() -> Result<Vec<Block>>;
-    
+
     /// This is to get the blocks without a parent
-    async fn read_blocks(filter: ReadBlockFilter) -> Result<Vec<Block>>;
-    
-    /// Update a block by passing the blocks to update 
+    async fn read_blocks(filter: &BlockQuery) -> Result<Vec<Block>>;
+
+    /// Update a block by passing the blocks to update
     async fn update_blocks(blocks: Vec<Block>) -> Result<Vec<Block>>;
-    
+
     /// Delete blocks by their ids
     /// Children blocks will be removed as well
     async fn delete_blocks(block_ids: Vec<String>) -> Result<Vec<Block>>;
 }
 
-/// Leve all empty to get root blocks
-/// One filter at a time
-pub struct ReadBlockFilter {
-    /// Get blocks by specifying their ids
-    pub ids: Vec<String>,
-    /// Speicify parent ids to get their children blocks
-    pub parent_ids: Vec<String>,
+pub enum BlockQuery {
+    Root,                    // blocks without parent
+    ByIds(Vec<String>),      // specific blocks
+    ChildrenOf(Vec<String>), // by parent ids
 }
