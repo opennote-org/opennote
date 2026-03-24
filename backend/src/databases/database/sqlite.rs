e74de26ef218dcbc3b71c8d75e10a5c1cce4b50c
Xinyu Bao <34500975+AspadaX@users.noreply.github.com>
2026-03-24 11:58:50 +0800

    implemented create_blocks

--- backend/src/databases/database/sqlite.rs
+++ backend/src/databases/database/sqlite.rs
@@ -3,12 +3,19 @@ use std::time::Duration;
 use anyhow::{Result, anyhow};
 use async_trait::async_trait;
 use migration::{Migrator, MigratorTrait};
-use sea_orm::{ConnectOptions, DatabaseConnection, EntityTrait};
+use sea_orm::{ActiveModelBehavior, ConnectOptions, DatabaseConnection, EntityTrait};
 
-use crate::{databases::database::{
-    metadata::MetadataSettings,
-    traits::{blocks::Blocks, database::Database, metadata::MetadataManagement},
-}, models::block::Block};
+use crate::{
+    databases::database::{
+        metadata::MetadataSettings,
+        traits::{
+            blocks::{BlockQuery, Blocks},
+            database::Database,
+            metadata::MetadataManagement,
+        },
+    },
+    models::block::Block,
+};
 
 #[derive(Debug, Clone)]
 pub struct SQLiteDatabase {
@@ -79,15 +86,22 @@ impl MetadataManagement for SQLiteDatabase {
 
 #[async_trait]
 impl Blocks for SQLiteDatabase {
-    async fn create_blocks() -> Result<Vec<Block>> {
-        use crate::entity::blocks::{Entity as BlockEntity, ActiveModel};
-        use crate::entity::payloads::Entity as PayloadEntity;
-        
-        BlockEntity::insert(
-            ActiveModel {}
-        );
-        
-        // create an empty block
-        // need to insert payloads later
+    async fn create_blocks(&self, num_blocks: usize) -> Result<Vec<Block>> {
+        use crate::entity::blocks::{ActiveModel, Entity as BlockEntity};
+
+        let blocks_active_models: Vec<ActiveModel> = (0..num_blocks)
+            .into_iter()
+            .map(|_| ActiveModel::new())
+            .collect();
+
+        let block = BlockEntity::insert_many(blocks_active_models)
+            .exec_with_returning(&self.pool)
+            .await?;
+
+        Ok(block.into_iter().map(|item| item.into()).collect())
+    }
+
+    async fn read_blocks(filter: &BlockQuery) -> Result<Vec<Block>> {
+        Ok(())
     }
 }
