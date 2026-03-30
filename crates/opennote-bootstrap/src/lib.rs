c264f5d9e49a098d646d690299d44b89c64f6308
Xinyu Bao <34500975+AspadaX@users.noreply.github.com>
2026-03-31 00:06:43 +0800

    Refactor bootstrap to use Arc for shared resources

--- crates/opennote-bootstrap/src/lib.rs
+++ crates/opennote-bootstrap/src/lib.rs
@@ -10,19 +10,19 @@ use opennote_tasks_scheduler::TasksScheduler;
 
 #[derive(Clone)]
 pub struct ApplicationBootStrap {
-    pub configurations: Configurations,
-    pub databases_layer_entry: Databases,
+    pub configurations: Arc<Configurations>,
+    pub databases: Databases,
     pub tasks_scheduler: Arc<Mutex<TasksScheduler>>,
-    pub embedder_entry: Arc<EmbedderEntry>,
+    pub embedders: Arc<EmbedderEntry>,
 }
 
 impl ApplicationBootStrap {
     pub async fn new(configurations: Configurations) -> Result<Self> {
         Ok(Self {
-            configurations: configurations.clone(),
+            configurations: Arc::new(configurations.clone()),
             tasks_scheduler: Arc::new(Mutex::new(TasksScheduler::new())),
-            databases_layer_entry: Databases::new(&configurations.system).await?,
-            embedder_entry: Arc::new(EmbedderEntry::new(&configurations.system).await?),
+            databases: Databases::new(&configurations.system).await?,
+            embedders: Arc::new(EmbedderEntry::new(&configurations.system).await?),
         })
     }
 }
