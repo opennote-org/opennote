3787c2ef693974d8dcb21699aed94d1d5ec94cae
Xinyu Bao <34500975+AspadaX@users.noreply.github.com>
2026-03-30 11:37:11 +0800

    Add bootstrap crate and refactor app initialization

--- crates/opennote-bootstrap/src/lib.rs
+++ crates/opennote-bootstrap/src/lib.rs
@@ -1,26 +1,27 @@
 use std::sync::Arc;
 
+use anyhow::Result;
 use tokio::sync::Mutex;
 
+use opennote_data::Databases;
 use opennote_embedder::entry::EmbedderEntry;
 use opennote_models::configurations::system::Config;
-
-use crate::entry::DatabasesLayerEntry;
+use opennote_tasks_scheduler::TasksScheduler;
 
 #[derive(Clone)]
-pub struct AppState {
+pub struct ApplicationBootStrap {
     pub config: Config,
-    pub databases_layer_entry: DatabasesLayerEntry,
+    pub databases_layer_entry: Databases,
     pub tasks_scheduler: Arc<Mutex<TasksScheduler>>,
     pub embedder_entry: Arc<EmbedderEntry>,
 }
 
-impl AppState {
-    pub async fn new(config: Config) -> anyhow::Result<Self> {
+impl ApplicationBootStrap {
+    pub async fn new(config: Config) -> Result<Self> {
         Ok(Self {
             config: config.clone(),
             tasks_scheduler: Arc::new(Mutex::new(TasksScheduler::new())),
-            databases_layer_entry: DatabasesLayerEntry::new(&config).await?,
+            databases_layer_entry: Databases::new(&config).await?,
             embedder_entry: Arc::new(EmbedderEntry::new(&config).await?),
         })
     }
