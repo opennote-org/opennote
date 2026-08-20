00b84bac7b116298395856b832dc963052fe1a43
Xinyu Bao <34500975+AspadaX@users.noreply.github.com>
2026-08-20 20:48:56 +0800

    make errors visible in the gui for the startup phase

--- crates/opennote-desktop/src/globals/velotype.rs
+++ crates/opennote-desktop/src/globals/velotype.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use gpui::App;
 
 use opennote_core_logics::configurations::{ApplicationType, get_configuration_folder_path};
@@ -10,18 +11,17 @@ use opennote_velotype::{
     theme::ThemeManager,
 };
 
-pub fn init_velotype(cx: &mut App) {
+pub fn init_velotype(cx: &mut App) -> Result<()> {
     let preferences = load_or_create_app_preferences_with_path(get_configuration_folder_path(
         ApplicationType::Desktop,
-    ))
-    .unwrap_or_else(|err| {
-        eprintln!("failed to initialize app preferences: {err}");
-        Default::default()
-    });
+    ))?;
+
     I18nManager::init_with_language_id(cx, &preferences.default_language_id);
     ThemeManager::init_with_theme_id(cx, &preferences.default_theme_id);
     EditorSettings::init(cx, preferences.show_table_headers);
     install_http_client(cx);
     init_with_keybindings(cx, &preferences.keybindings);
     app_menu::init(cx);
+
+    Ok(())
 }
